e5eac897535fdaae788bb1e8d320e4f84686d079
MD. Jahid Hasan <jh18186676@gmail.com>
2025-07-11 10:27:26 +0600

    admin delete user

--- src/app/api/allUsers/route.js
+++ src/app/api/allUsers/route.js
@@ -2,18 +2,41 @@ import { NextResponse } from "next/server";
 import connectionToDatabase from "../../../../lib/db";
 import User from "../../../../models/user";
 
-export async function GET(req){
-   
-        await connectionToDatabase();
-        const url = new URL(req.url);
-        const adminEmail = url.searchParams.get("adminEmail");
+export async function GET(req) {
 
-     try{
-        const users = await User.find({email: {$ne: adminEmail}});
-        return NextResponse.json(users, {status: 200})
+    await connectionToDatabase();
+    const url = new URL(req.url);
+    const adminEmail = url.searchParams.get("adminEmail");
 
-    }catch(error){
-        return NextResponse.json({message: "Server Error"}, {status: 500})
+    try {
+        const users = await User.find({ email: { $ne: adminEmail } });
+        return NextResponse.json(users, { status: 200 })
+
+    } catch (error) {
+        return NextResponse.json({ message: "Server Error" }, { status: 500 })
     }
+
+}
+
+export async function DELETE(req) {
+    await connectionToDatabase();
+    try {
+        const body = await req.json();
+       //console.log("body", body)
+        const { userId } = body;
+        //console.log("UserId", userId)
+
+        const deletedUser = await User.findByIdAndDelete(userId);
+        //console.log("deleted user", deletedUser)
+
+        if (!deletedUser) {
+            return NextResponse.json({ message: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: 'User deleted successfully' }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Server error', error }, { status: 500 });
+    }
+
 
 }
--- src/components/dashboard-ui/admin-ui/AllUsers.jsx
+++ src/components/dashboard-ui/admin-ui/AllUsers.jsx
@@ -1,13 +1,16 @@
 "use client";
+import useRole from "@/components/client-hooks/useRole";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import React from "react";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const ResponsiveTable = () => {
 
     const { data: session } = useSession();
+    const {role} = useRole();
 
     const { data: allUsers = [], isLoading, error, refetch } = useQuery({
         queryKey: ["allUsers", session?.user?.email],
@@ -17,7 +20,43 @@ const ResponsiveTable = () => {
         }
     })
     if (isLoading) return <h2>Loading...</h2>
-    
+
+    // product delete from database
+    const handleUserDelete = async (id) => {
+         console.log("id", id)
+
+        try {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    const res = await axios.delete("/api/allUsers", {data: {userId: id}})
+                    console.log("res", res)
+                    if (res?.status === 200) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: `user Successfully Deleted.`,
+                            icon: "success"
+                        });
+                        refetch();
+                    }
+
+                }
+            });
+            
+
+        } catch (error) {
+            console.error("Delete failed:", error.response?.data?.message || error.message);
+        }
+    }
+
+
 
     return (
         <div className="w-11/12 mx-auto py-10">
@@ -34,14 +73,15 @@ const ResponsiveTable = () => {
                     </thead>
                     <tbody className="divide-y divide-gray-200 text-[16px]  text-gray-800">
                         {allUsers.map((user, index) => (
-                            <tr key={user._id} className="hover:bg-gray-50">
+                            <tr key={user?._id} className="hover:bg-gray-50">
                                 <td className="px-6 py-4">{index + 1}</td>
-                                <td className="px-6 py-4">{user.name}</td>
-                                <td className="px-6 py-4">{user.email}</td>
-                                <td className="px-6 py-4">{user.role}</td>
+                                <td className="px-6 py-4">{user?.name}</td>
+                                <td className="px-6 py-4">{user?.email}</td>
+                                <td className="px-6 py-4">{user?.role}</td>
 
                                 <td className="px-6 py-4">
                                     <button
+                                        onClick={()=>handleUserDelete(user?._id)}
                                         className="bg-red-500 text-white px-2 py-2 rounded-md hover:bg-red-600 cursor-pointer">
                                         <MdDelete />
                                     </button>
